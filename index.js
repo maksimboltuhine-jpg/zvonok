25ff2035d6b24050d2b353c36a2e44507aee3676
maksimboltuhine-jpg <maksimboltuhine@gmail.com>
2026-04-05 21:25:42 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-// Раздаем статику (наш html)
 app.use(express.static(__dirname));
 
-// Настройка Peer сервера
 const peerServer = ExpressPeerServer(server, {
 debug: true,
-path: '/'
+path: '/',
+allow_discovery: true
 });
 
 app.use('/peerjs', peerServer);
 
-app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Лог для отладки в PowerShell
+peerServer.on('connection', (client) => { console.log(`Юзер зашел: ${client.getId()}`); });
+peerServer.on('disconnect', (client) => { console.log(`Юзер вышел: ${client.getId()}`); });
 
-// Порт 10000 идеален для Render
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
-console.log(`=== SERVER RUNNING ON PORT ${PORT} ===`);
+console.log(`=== SERVER v26 ONLINE ON PORT ${PORT} ===`);
 });
