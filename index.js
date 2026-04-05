d9048371a9cc3c11951fc319b73cdad9e155fcbc
maksimboltuhine-jpg <maksimboltuhine@gmail.com>
2026-04-05 22:09:54 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -16,13 +16,11 @@ allow_discovery: true
 
 app.use('/peerjs', peerServer);
 
-// Лог для отладки в PowerShell
-peerServer.on('connection', (client) => { console.log(`Юзер зашел: ${client.getId()}`); });
-peerServer.on('disconnect', (client) => { console.log(`Юзер вышел: ${client.getId()}`); });
-
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
-console.log(`=== SERVER v26 ONLINE ON PORT ${PORT} ===`);
+console.log(`=== SERVER ONLINE ON PORT ${PORT} ===`);
 });
