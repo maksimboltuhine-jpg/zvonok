6b17a35a4242e0d0a46a18af795a5f992bfb31f2
maksimboltuhine-jpg <maksimboltuhine@gmail.com>
2026-04-05 20:25:15 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ res.sendFile(path.join(__dirname, 'index.html'));
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
-console.log(`Сервер запущен на порту ${PORT}`);
+console.log(`Server started on port ${PORT}`);
 });
